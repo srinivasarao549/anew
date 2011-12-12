eff7b71eb6e0c0a25ca44652e6cc68be56416cc2
hughfdjackson <hughfdjackson@googlemail.com>
2011-12-12 17:56:28 +0000

    style changes

--- src/anew.js
+++ src/anew.js
@@ -32,18 +32,18 @@ void function(init){
             }
             
 
-            var new_object = Object.create(proto)
+            var return_object = Object.create(proto)
             
             // mixin extra props
-            mixin_object(new_object, object)
+            mixin_object(return_object, object)
 
             // call all inits in prototype
-            if ( proto instanceof Object ) call_proto_inits(new_object)
+            if ( proto instanceof Object ) call_proto_inits(return_object)
            
             // call init that's been mixed in, if any (call in case of null proto)
-            if ( {}.hasOwnProperty.call(new_object, init) ) new_object[init]()
+            if ( {}.hasOwnProperty.call(return_object, init) ) return_object[init]()
             
-            return new_object
+            return return_object
         }
     
     // export
