0d05a48ca8739b126a60ea0f09d529b58784ec3d
hughfdjackson <hughfdjackson@googlemail.com>
2011-12-12 17:55:48 +0000

    style change

--- src/anew.js
+++ src/anew.js
@@ -3,7 +3,7 @@ void function(init){
     var get_proto = Object.getPrototypeOf,
         anew = function(proto, object){
             
-            !function set_defaults(){
+            void function set_defaults(){
                 if ( proto === undefined ) proto = {}
                 if ( object === undefined ) object = {}
             }()
